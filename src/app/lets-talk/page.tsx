59749bfabb145b9194919c268d54ebcbef92fbb6
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-05-06 18:52:43 -0500

    refactor: Update LetsTalk component to use lazy loading for images

--- src/app/lets-talk/page.tsx
+++ src/app/lets-talk/page.tsx
@@ -1,14 +1,15 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import LetsTalk from '@/components/LetsTalk';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 function Page() {
-
   return (
     <div className="h-screen">
       <Navbar />
-      <LetsTalk />
+      <Suspense>
+        <LetsTalk />
+      </Suspense>
       <hr className="h-0.5 bg-primary" />
       <Footer />
     </div>
